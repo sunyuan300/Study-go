b4f73ae4b272851edb57c9c67bc7f68f42dec9c0
80302367 <sunyuan@oppo.com>
2021-10-18 10:22:41 +0800

    gin&opentracing

--- apm/tracing/gin/cmd/cmd.go
+++ apm/tracing/gin/cmd/cmd.go
@@ -1,1 +1,50 @@
-package cmd
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"stdlib_learn/apm/tracing/config"
+	"stdlib_learn/apm/tracing/gin/middleware"
+	"stdlib_learn/apm/tracing/gin/router"
+	"time"
+)
+
+func main()  {
+	config.NewTracer("gin-demo")
+
+	e := gin.New()
+	e.Use(middleware.Jaeger())
+	router.Register(e)
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: e,
+		ReadTimeout: 15*time.Second,
+		WriteTimeout: 15*time.Second,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("HTTP server listen:%s\n",err)
+		}
+	}()
+
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+	log.Println("Get Signal:", sig)
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
+
+}
--- apm/tracing/gin/middleware/jaeger.go
+++ apm/tracing/gin/middleware/jaeger.go
@@ -1,1 +1,32 @@
 package middleware
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+func Jaeger() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var rootSpan opentracing.Span
+		// 直接从 ctx.Request.Header 中提取span,如果没有就新建一个
+		spCtx,err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders,opentracing.HTTPHeadersCarrier(ctx.Request.Header))
+		if err != nil {
+			rootSpan = opentracing.GlobalTracer().StartSpan(ctx.Request.URL.Path)
+			defer rootSpan.Finish()
+		} else {
+			rootSpan = opentracing.StartSpan(
+				ctx.Request.URL.Path,
+				opentracing.ChildOf(spCtx),
+				opentracing.Tag{Key: string(ext.Component),Value: "HTTP"},
+				ext.SpanKindRPCServer,
+				)
+			defer rootSpan.Finish()
+		}
+
+		ctx.Set("tracer",opentracing.GlobalTracer())
+		ctx.Set("ctx",opentracing.ContextWithSpan(context.Background(),rootSpan))
+		ctx.Next()
+	}
+}
