59d3f041d154106265288887653cd922e4e509ef
SunYuan <sysysy300@163.com>
2022-01-09 15:33:53 +0800

    feat:GPM

--- test/main.go
+++ test/main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	_ "net/http/pprof"
+	"time"
 )
 
-type Order struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
 func main() {
-	o := Order{
-		ID:   "123",
-		Name: "abc",
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second)
+		fmt.Println("Hello scheduler")
 	}
-	b, err := json.Marshal(o)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(b))
 }
